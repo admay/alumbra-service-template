dde355eedeefff37327f363c29d5b29377cfae21
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-22 14:07:05 -0400

    Added rendering

--- src/leiningen/new/alumbra_service.clj
+++ src/leiningen/new/alumbra_service.clj
@@ -5,10 +5,28 @@
 (def render (renderer "alumbra-service"))
 
 (defn alumbra-service
-  "FIXME: write documentation"
-  [name]
+  [name & args]
   (let [data {:name name
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' alumbra-service project.")
     (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+
+             ;; root
+             ["README.md" (render "README.md" data)]
+             ["project.clj" (render "project.clj" data)]
+             ["profiles.clj" (render "profiles.clj" data)]
+             [".gitignore" (render ".gitignore" data)]
+
+             ;; src
+             ["src/{{sanitized}}/database.clj" (render "database.clj" data)]
+             ["src/{{sanitized}}/resolvables.clj" (render "resolvables.clj" data)]
+             ["src/{{sanitized}}/root.clj" (render "root.clj" data)]
+             ["src/{{sanitized}}/server.clj" (render "server.clj" data)]
+             ["src/{{sanitized}}/specs.clj" (render "specs.clj" data)]
+             ["src/{{sanitized}}/utils.clj" (render "utils.clj" data)]
+
+             ;; test
+
+             ;; resources
+
+             )))
