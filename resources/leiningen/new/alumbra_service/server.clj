7d678471353ac2f39316726b30e1a02500648420
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-23 10:51:02 -0400

    Added context fn, changed routes to use root

--- resources/leiningen/new/alumbra_service/server.clj
+++ resources/leiningen/new/alumbra_service/server.clj
@@ -1,22 +1,27 @@
 (ns {{name}}.server
     (:require
      [clojure.java.io :as io]
-     [compojure.core :refer :all]
+     [compojure.core :refer [GET POST defroutes]]
      [compojure.route :as route]
      [compojure.handler :as handler]
      [alumbra.core :as alumbra]
      [alumbra.web.graphiql-workspace :as graphiql]
+     [environ.core :refer [env]]
      [{{name}}.root :refer [QueryRoot MutationRoot]]))
+
+(defn context []
+  {:db (env :database-url)})
 
 (def graphql-handler
   (alumbra/handler
    {:schema (io/resource "schema.graphql")
     :query QueryRoot
-    :mutation MutationRoot}))
+    :mutation MutationRoot
+    :context-fn (context)}))
 
 (defroutes app-routes
-  (GET "/graphql" request (graphiql/handler request))
-  (POST "graphql" request (graphql-handler request))
+  (GET "/" request (graphiql/handler request))
+  (POST "/graphql" request (graphql-handler request))
   (route/not-found "Page not found\n"))
 
 (def app (handler/api app-routes))
