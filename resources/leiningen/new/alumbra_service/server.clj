8fa72a6b104096aa5eceb82284310ded3363aaa3
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-22 21:56:27 -0400

    Added code for server

--- resources/leiningen/new/alumbra_service/server.clj
+++ resources/leiningen/new/alumbra_service/server.clj
@@ -1,2 +1,21 @@
-(ns {{name}}.server)
+(ns {{name}}.server
+    (:require
+     [compojure.core :refer :all]
+     [compojure.route :as route]
+     [compojure.handler :as handler]
+     [alumbra.core :as alumbra]
+     [alumbra.web.graphiql-workspace :as graphiql]
+     [{{name}}.root :refer [QueryRoot MutationRoot]]))
 
+(def graphql-handler
+  (alumbra/handler
+   {:schema schema
+    :query QueryRoot
+    :mutation MutationRoot}))
+
+(defroutes app-routes
+  (GET "/graphql" request (graphiql/handler request))
+  (POST "graphql" request (graphql-handler request))
+  (route/not-found "Page not found\n"))
+
+(def app (handler/api app-routes))
