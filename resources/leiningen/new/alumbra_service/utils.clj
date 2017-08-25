bb5e79a606f3f195b7077978bafa0f7a6b324f4f
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-25 13:57:38 -0400

    Added utils ns

---
+++ resources/leiningen/new/alumbra_service/utils.clj
@@ -0,0 +1,24 @@
+(ns {{name}}.utils
+    (:require
+     [clojure.walk :as walk]
+     [clojure.string :refer [replace]]))
+
+(defn transform-keys
+  "Recursively transforms all map keys in coll with t."
+  [t coll]
+  (let [f (fn [[k v]] [(t k) v])]
+    (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) coll)))
+
+(defn underscore->hyphen
+  [s]
+  (keyword (string/replace (string/replace s #"_" "-") #":" "")))
+
+(defn hyphen->underscore
+  [s]
+  (string/replace (string/replace s #"-" "_") #":" ""))
+
+(defn row->resolvable [row]
+  (transform-keys underscore->hyphen row))
+
+
+
