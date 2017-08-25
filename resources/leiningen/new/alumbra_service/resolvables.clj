18d65746e39695423cd8b3d9e74cc13a0f6e5609
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-25 13:57:20 -0400

    Updated resolvables to match new schema

--- resources/leiningen/new/alumbra_service/resolvables.clj
+++ resources/leiningen/new/alumbra_service/resolvables.clj
@@ -1,53 +1,35 @@
 (ns {{name}}.resolvables
-    (:require
-     [claro.data :as data]
-     [{{name}}.database :as db]))
+  (:require
+   [claro.data :as data]
+   [manifold.deferred :as d]
+   [clojure.walk :as walk]
+   [{{name}}.database :refer :all]))
 
-(declare ->PersonById)
+;; Queries
+(defrecord Person [id]
+  data/Resolvable
+  (resolve! [_ {:keys [db] :as env}]
+    (d/future
+      (fetch-person db id))))
 
-(defrecord Person
-    [id first-name last-name]
-    data/Resolvable
+(defrecord Property [id]
+  data/Resolvable
+  (resolve! [_ {:keys [db] :as env}]
+    (d/future
+      (fetch-property db id))))
+
+(defrecord Properties [owner]
+  data/Resolvable
+  (resolve! [_ {:keys [db] :as env}]
+    (map map->Property (fetch-properties db owner))))
+
+;; Mutations
+(defrecord CreatePerson [data]
+  data/Resolvable
   (resolve! [_ env]
-    {:id id
-     :first-name first-name
-     :last-name last-name}))
+    (map->Person (create-person! (:database-url env) data))))
 
-(defrecord Property
-    [id owner-id address size]
-    data/Resolvable
+(defrecord CreateProperty [data]
+  data/Resolvable
   (resolve! [_ env]
-    {:id id
-     :owner (->PersonById owner-id)
-     :address address
-     :size size}))
-
-(defrecord PersonById
-    [id]
-    data/Resolvable
-  (resolve! [_ env]
-    (map->Person (db/fetch-person-by-id (:database-url env) id))))
-
-(defrecord PropertyById
-    [id]
-    data/Resolvable
-  (resolve! [_ env]
-    (map->Property (db/fetch-property-by-id (:database-url env) id))))
-
-(defrecord Properties
-    [owner-id]
-    data/Resolvable
-  (resolve! [_ env]
-    (map map->Property (db/fetch-properties-by-owner (:database-url env) owner-id))))
-
-(defrecord CreatePerson
-    [data]
-    data/Resolvable
-  (resolve! [_ env]
-    (map->Person (db/create-person! (:database-url env) data))))
-
-(defrecord CreateProperty
-    [data]
-    data/Resolvable
-  (resolve! [_ env]
-    (map->Property (db/create-property! (:database-url env) data))))
+    (map->Property (create-property! (:database-url env) data))))
